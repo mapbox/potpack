834a48ef13eaefc691194d7cf40b00e6e3b9b699
Vladimir Agafonkin <agafonkin@gmail.com>
2020-07-13 16:10:59 +0300

    fix tests

--- test.mjs
+++ test.mjs
@@ -4,14 +4,16 @@ import test from 'tape';
 
 test('packs rectangles properly', (t) => {
     const boxes = [];
-    for (let i = 0; i < 100; i++) {
-        boxes.push({w: i, h: i});
+    for (let i = 1; i < 100; i++) {
+        boxes.push({w: i, h: i, i});
     }
     const {w, h, fill} = potpack(boxes);
 
     t.equal(w, 588);
     t.equal(h, 595);
     t.ok(fill > 0.93);
+
+    t.same(boxes.find(b => b.i === 1), {w: 1, h: 1, i: 1, x: 586, y: 416});
 
     t.end();
 });
