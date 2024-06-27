763aa5ee3595426358f9d9ed4c9da3e683731188
Vladimir Agafonkin <agafonkin@gmail.com>
2024-06-27 15:23:25 +0300

    switch to GH actions, ESLint 9+, NPM

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
 
 import potpack from './index.js';
-import test from 'tape';
+import test from 'node:test';
+import assert from 'node:assert/strict';
 
-test('packs rectangles properly', (t) => {
+test('packs rectangles properly', () => {
     const boxes = [];
     for (let i = 1; i < 100; i++) {
         boxes.push({w: i, h: i, i});
     }
     const {w, h, fill} = potpack(boxes);
 
-    t.equal(w, 588);
-    t.equal(h, 595);
-    t.ok(fill > 0.93);
+    assert.equal(w, 588);
+    assert.equal(h, 595);
+    assert.ok(fill > 0.93);
 
-    t.same(boxes.find(b => b.i === 1), {w: 1, h: 1, i: 1, x: 586, y: 416});
-
-    t.end();
+    assert.deepEqual(boxes.find(b => b.i === 1), {w: 1, h: 1, i: 1, x: 586, y: 416});
 });
