5d2c35cd3eaf4f893478be53124ee777121640b8
SuperLlama88888 <116475184+SuperLlama88888@users.noreply.github.com>
2025-07-14 17:53:51 +1000

    Remove "declare module" in index.d.ts (#11)

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,37 @@
-declare module "potpack" {
-  export interface PotpackBox {
-    w: number;
-    h: number;
-    /**
-     * X coordinate in the resulting container.
-     */
-    x?: number;
-    /**
-     * Y coordinate in the resulting container.
-     */
-    y?: number;
-  }
-
-  interface PotpackStats {
-    /**
-     * Width of the resulting container.
-     */
-    w: number;
-    /**
-     * Height of the resulting container.
-     */
-    h: number;
-    /**
-     * The space utilization value (0 to 1). Higher is better.
-     */
-    fill: number;
-  }
-
+export interface PotpackBox {
+  w: number;
+  h: number;
   /**
-   * Packs 2D rectangles into a near-square container.
-   *
-   * Mutates the {@link boxes} array: it's sorted by height,
-   * and box objects are augmented with `x`, `y` coordinates.
+   * X coordinate in the resulting container.
    */
-  const potpack: (boxes: PotpackBox[]) => PotpackStats;
-
-  export default potpack;
+  x?: number;
+  /**
+   * Y coordinate in the resulting container.
+   */
+  y?: number;
 }
+
+interface PotpackStats {
+  /**
+   * Width of the resulting container.
+   */
+  w: number;
+  /**
+   * Height of the resulting container.
+   */
+  h: number;
+  /**
+   * The space utilization value (0 to 1). Higher is better.
+   */
+  fill: number;
+}
+
+/**
+ * Packs 2D rectangles into a near-square container.
+ *
+ * Mutates the {@link boxes} array: it's sorted by height,
+ * and box objects are augmented with `x`, `y` coordinates.
+ */
+declare const potpack: (boxes: PotpackBox[]) => PotpackStats;
+
+export default potpack;
